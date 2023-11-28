8902f30af8dbf1d0813e6b07e23d286180d88889
arshadhossainoh <arshadhossain.oh@gmail.com>
2023-11-28 14:51:22 -0500

    broke down restaurant.ejs file and made it dynamic

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
+const uuid = require("uuid");
 const { json } = require("body-parser");
 const app = express();
 
@@ -32,12 +33,29 @@ app.get("/restaurants", (req, res) => {
   });
 });
 
+// dynamic route, id will be used to identify each
+// Req.params property is an object containing properties mapped to the named route “parameters”. For example, if you have the route /user/:name, then the “name” property is available as req.params.name. This object defaults to {}.
+//Req.body contains key-value pairs of data submitted in the request body
+app.get("/restaurants/:id", (req, res) => {
+  const restaurantId = req.params.id;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render("restaurant-detail", { rid: restaurantId });
+    }
+  }
+});
+
 app.get("/recommend", (req, res) => {
   //const htmlFilePath = path.join(__dirname, "views", "recommend.html");
   res.render("recommend");
 });
 app.post("/recommend", (req, res) => {
   const restaurant = req.body;
+  restaurant.id = uuid.v4();
   const filePath = path.join(__dirname, "data", "restaurants.json");
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
@@ -48,7 +66,7 @@ app.post("/recommend", (req, res) => {
 });
 app.get("/confirm", (req, res) => {
   //const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  res.redirect("/confirm");
+  res.render("confirm");
 });
 app.get("/about", (req, res) => {
   //const htmlFilePath = path.join(__dirname, "views", "about.html");
